d0a27aca531a11907d1679e7d43bd6a8d7cf4705
Marcus Libutan <tmlibutan@gmail.com>
2016-12-17 05:31:40 -0800

    Initial commit

---
+++ p6601-vendor-blobs.mk
@@ -0,0 +1,12 @@
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blu/p6601/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blu/p6601/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blu/p6601/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blu/p6601/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blu/p6601/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/blu/p6601/vendor,system/vendor)
